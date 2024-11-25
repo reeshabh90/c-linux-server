ff66e646a90a3bf6581fa89fbf123e0043463a28
reeshabh90 <reeshabh.007@gmail.com>
2024-11-25 17:07:31 +0530

    Fixing small issues

--- FTP/ftp_simple_server.c
+++ FTP/ftp_simple_server.c
@@ -13,7 +13,7 @@
 #define DATA_PORT 2021
 #define USERNAME "ftpClient"
 #define PASSWORD "ftpCLPass"
-#define CLIENT_HOME_DIR "/home/ftpuser"  // User's home directory
+#define CLIENT_HOME_DIR "/home/ftpuser" // User's home directory
 
 void *handle_client(void *arg);
 void list_directory(const char *dir_path, int data_socket);
@@ -68,8 +68,9 @@ int main(int argc, char const *argv[])
 
         // Handle each client in a new thread
         pthread_t client_thread;
-        int *new_sock = malloc(1);
+        int *new_sock = malloc(sizeof(int));
         *new_sock = ftp_control_socket;
+        // pthread_create requires the argument to be a void *
         if (pthread_create(&client_thread, NULL, handle_client, (void *)new_sock) < 0)
         {
             perror("Could not create thread");
@@ -85,6 +86,7 @@ int main(int argc, char const *argv[])
 
 void *handle_client(void *arg)
 {
+    // casting the recieved argument back to integer
     int ftp_control_socket = *(int *)arg;
     free(arg);
     char buffer[BUFFER_SIZE];
@@ -95,7 +97,8 @@ void *handle_client(void *arg)
     memset(buffer, 0, BUFFER_SIZE);
     read(ftp_control_socket, buffer, BUFFER_SIZE);
     printf("Received Username: %s", buffer);
-    if (strncmp(buffer, USERNAME, strlen(USERNAME)) != 0) {
+    if (strncmp(buffer, USERNAME, strlen(USERNAME)) != 0)
+    {
         const char *response = "530 Invalid username.\n";
         write(ftp_control_socket, response, strlen(response));
         close(ftp_control_socket);
@@ -104,11 +107,12 @@ void *handle_client(void *arg)
     const char *username_ok = "331 Username OK, need password.\n";
     write(ftp_control_socket, username_ok, strlen(username_ok));
 
-        // Step 2: Receive Password
+    // Step 2: Receive Password
     memset(buffer, 0, BUFFER_SIZE);
     read(ftp_control_socket, buffer, BUFFER_SIZE);
     printf("Received Password: %s", buffer);
-    if (strncmp(buffer, PASSWORD, strlen(PASSWORD)) != 0) {
+    if (strncmp(buffer, PASSWORD, strlen(PASSWORD)) != 0)
+    {
         const char *response = "530 Login incorrect.\n";
         write(ftp_control_socket, response, strlen(response));
         close(ftp_control_socket);
@@ -122,17 +126,25 @@ void *handle_client(void *arg)
     read(ftp_control_socket, buffer, BUFFER_SIZE);
     printf("Received: %s\n", buffer);
 
-    if (strncmp(buffer, "PORT", 4) == 0) {
+    if (strncmp(buffer, "PORT", 4) == 0)
+    {
         int p1, p2;
         char ip[16];
+        // sample: "PORT 127,0,0,1,192,168\n"
+        // The first 5 characters "PORT " are skipped
+        // "%[^,]": This will read "127.0.0.1" (up to the first comma) into the ip variable.
+        // "%d": This will read 192 into the p1 variable.
+        // "%d": This will read 168 into the p2 variable.
         sscanf(buffer + 5, "%[^,],%d,%d", ip, &p1, &p2);
+        // Rebuilding the Data Port, This would be the port on which the server will try to connect to the client for data transfer.
         int client_data_port = p1 * 256 + p2;
 
         printf("Client data IP: %s, Port: %d\n", ip, client_data_port);
 
         // Connect to the client data port
         ftp_data_socket = socket(AF_INET, SOCK_STREAM, 0);
-        if (ftp_data_socket < 0) {
+        if (ftp_data_socket < 0)
+        {
             perror("Data socket creation failed");
             close(ftp_control_socket);
             return NULL;
@@ -143,7 +155,8 @@ void *handle_client(void *arg)
         client_data_addr.sin_addr.s_addr = inet_addr(ip);
         client_data_addr.sin_port = htons(client_data_port);
 
-        if (connect(ftp_data_socket, (struct sockaddr *)&client_data_addr, sizeof(client_data_addr)) < 0) {
+        if (connect(ftp_data_socket, (struct sockaddr *)&client_data_addr, sizeof(client_data_addr)) < 0)
+        {
             perror("Data connection failed");
             close(ftp_data_socket);
             close(ftp_control_socket);
@@ -157,14 +170,17 @@ void *handle_client(void *arg)
         read(ftp_control_socket, buffer, BUFFER_SIZE);
         printf("Received Command: %s", buffer);
 
-        if (strncmp(buffer, "LIST", 4) == 0) {
-            const char *data = "file1.txt\nfile2.txt\n";
-            write(ftp_data_socket, data, strlen(data));
+        if (strncmp(buffer, "LIST", 4) == 0)
+        {
+            // List files from the user's home directory (e.g., /home/ftpuser)
+            list_directory(CLIENT_HOME_DIR, ftp_data_socket);
             close(ftp_data_socket);
 
             const char *response = "226 Directory send OK.\n";
             write(ftp_control_socket, response, strlen(response));
-        } else {
+        }
+        else
+        {
             const char *error_response = "502 Command not implemented.\n";
             write(ftp_control_socket, error_response, strlen(error_response));
         }
@@ -175,21 +191,25 @@ void *handle_client(void *arg)
     return NULL;
 }
 
-void list_directory(const char *dir_path, int data_socket) {
+void list_directory(const char *dir_path, int data_socket)
+{
     DIR *dir;
     struct dirent *entry;
     char buffer[BUFFER_SIZE];
 
     dir = opendir(dir_path);
-    if (dir == NULL) {
+    if (dir == NULL)
+    {
         perror("opendir failed");
         return;
     }
 
     // Read all entries in the directory and send them to the client
-    while ((entry = readdir(dir)) != NULL) {
+    while ((entry = readdir(dir)) != NULL)
+    {
         // Skip hidden files (files starting with .)
-        if (entry->d_name[0] != '.') {
+        if (entry->d_name[0] != '.')
+        {
             snprintf(buffer, sizeof(buffer), "%s\n", entry->d_name);
             write(data_socket, buffer, strlen(buffer));
         }
